16f4c01df1c38c80adf1c9d803b2fccea2432e49
CameronJYoung <cameronjyoung123@gmail.com>
2021-03-07 03:32:55 +0000

    commited unsaved data

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+
+// Initialize app
+const app = express()
+
+// Parse requests of content type: application/json
+app.use(bodyParser.json)
