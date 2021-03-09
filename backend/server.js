395f3db0d280ef83b533246f7d0cbad1770f2d1f
CameronJYoung <cameronjyoung123@gmail.com>
2021-03-09 01:54:10 +0000

    added some routes, started adding database connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const getUserById = require('./app/routes/getUserById');
+
 
 // Initialize app
 const app = express()
 
 // Parse requests of content type: application/json
 app.use(bodyParser.json)
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/getUserById', getUserById)
+
+app.listen('2242', () => {
+    console.log(`App running on port 2242.`)
+})
