aa0759dad1aa4fcf8a4a62e7a90ec27ad1a994dd
CameronJYoung <cameronjyoung123@gmail.com>
2021-03-15 13:48:38 +0000

    frontend fixes

--- frontend/src/pages/Signup/Signup.js
+++ frontend/src/pages/Signup/Signup.js
@@ -3,9 +3,11 @@ import React from "react";
 export default class Signup extends React.Component {
 	render() {
 		return (
-            <>
-				
-            </>
+			<div className="pageContainer">
+				<div className="pageTitleContainer">
+					<h1>Sign Up</h1>
+				</div>
+			</div>
 		)
 	}
 }
