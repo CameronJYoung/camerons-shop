6014d23c1605ed768eb8fbe686924c82398fca19
CameronJYoung <cameronjyoung123@gmail.com>
2021-03-15 17:26:04 +0000

    added sequelize models to adminbro panel

--- backend/app/routes/admin.routes.js
+++ backend/app/routes/admin.routes.js
@@ -2,9 +2,14 @@ module.exports = app => {
 
     const AdminBro = require('admin-bro')
     const AdminBroExpress = require('@admin-bro/express')
+    const AdminBroSequelize = require('@admin-bro/sequelize')
+
+    AdminBro.registerAdapter(AdminBroSequelize)
     
+    const db = require('../models')
+
     const adminBro = new AdminBro({
-        databases: [],
+        databases: [db],
         rootPath: '/admin',
     })
     
