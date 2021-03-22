9928b517f64d76076a196dfb79bdd8eaf65b02e2
CameronJYoung <cameronjyoung123@gmail.com>
2021-03-22 22:31:57 +0000

    completed hamburger navigation functionality

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -5,17 +5,32 @@ import './Navigation.css';
 import {Link} from 'react-router-dom';
 
 export default class Navigation extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {isNavToggled: false}
+
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav() {
+        console.log(1);
+        this.setState(state => ({
+            isNavToggled: !state.isNavToggled
+        }));  
+    }
+
 	render() {
 		return (
             <header className="mainHeader">
+                <button onClick={this.toggleNav} className="toggleButton">
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </button>
                 <img className="headerLogo" src={'https://dummyimage.com/500x200/000/fff&text=(500x200)/2'} alt="Logo" />
-                <a href="#" className="toggleButton">
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                </a>
                 <nav className="mainNav">
-                    <ul>
+                    <ul className={this.state.isNavToggled ? 'active' : null}>
                         <li>
                             <Link to="/">Home</Link>
                         </li>
